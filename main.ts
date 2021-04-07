b7f3d00309080fad19ac250cde4ac9c26c58b049
nicolachinellato <78468160+nicolachinellato@users.noreply.github.com>
2021-04-07 08:33:58 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+scene.setBackgroundColor(9)
+let Hero = sprites.create(img`
+    . . . . . . . . . . . . 
+    . . . f f f f f f . . . 
+    . f f f e e e e e f . . 
+    f f f e e e e e e e f . 
+    f f f f e e e e e e e f 
+    f f f f f e e e 4 e e f 
+    f f f f e e e 4 4 e e f 
+    f f f f 4 4 4 4 4 e f f 
+    f f 4 e 4 f f 4 4 e f . 
+    f f 4 d 4 d d d d f . . 
+    . f f f 4 d d b b f . . 
+    . 4 d d e 4 4 4 e f . . 
+    . e d d e 1 1 1 1 f . . 
+    . f e e f 6 6 6 6 f f . 
+    . f f f f f f f f f f . 
+    . . f f . . . f f f . . 
+    `, SpriteKind.Player)
+controller.moveSprite(Hero)
+tiles.setTilemap(tilemap`level1`)
