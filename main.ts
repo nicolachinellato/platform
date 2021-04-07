591bd55967cb82dc1c43186cde8b137cc683bbd8
nicolachinellato <78468160+nicolachinellato@users.noreply.github.com>
2021-04-07 08:59:33 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,128 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    // Gestisci salto
+    Hero.vy = -100
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    Hero,
+    [img`
+        . . . f f f f f . . . . 
+        . . f e e e e e f f . . 
+        . f e e e e e e e f f . 
+        f e e e e e e e f f f f 
+        f e e 4 e e e f f f f f 
+        f e e 4 4 e e e f f f f 
+        f f e 4 4 4 4 4 f f f f 
+        f f e 4 4 f f 4 e 4 f f 
+        . f f d d d d 4 d 4 f . 
+        . . f b b d d 4 f f f . 
+        . . f e 4 4 4 e e f . . 
+        . . f 1 1 1 e d d 4 . . 
+        . . f 1 1 1 e d d e . . 
+        . . f 6 6 6 f e e f . . 
+        . . . f f f f f f . . . 
+        . . . . . f f f . . . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . . f e e e e e f f f . 
+        . f e e e e e e e f f f 
+        f e e e e e e e f f f f 
+        f e e 4 e e e f f f f f 
+        f e e 4 4 e e e f f f f 
+        f f e 4 4 4 4 4 f f f f 
+        . f e 4 4 f f 4 e 4 f f 
+        . . f d d d d 4 d 4 f . 
+        . . f b b d e e f f f . 
+        . . f e 4 e d d 4 f . . 
+        . . f 1 1 e d d e f . . 
+        . f f 6 6 f e e f f f . 
+        . f f f f f f f f f f . 
+        . . f f f . . . f f . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . . f e e e e e f f f . 
+        . f e e e e e e e f f f 
+        f e e e e e e e f f f f 
+        f e e 4 e e e f f f f f 
+        f e e 4 4 e e e f f f f 
+        f f e 4 4 4 4 4 f f f f 
+        . f e 4 4 f f 4 e 4 f f 
+        . . f d d d d 4 d 4 f f 
+        . . f b b d d 4 f f f . 
+        . . f e 4 4 4 e d d 4 . 
+        . . f 1 1 1 1 e d d e . 
+        . f f 6 6 6 6 f e e f . 
+        . f f f f f f f f f f . 
+        . . f f f . . . f f . . 
+        `],
+    100,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    Hero,
+    [img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . f f f e e e e e f . . 
+        f f f e e e e e e e f . 
+        f f f f e e e e e e e f 
+        f f f f f e e e 4 e e f 
+        f f f f e e e 4 4 e e f 
+        f f f f 4 4 4 4 4 e f f 
+        f f 4 e 4 f f 4 4 e f . 
+        f f 4 d 4 d d d d f . . 
+        . f f f 4 d d b b f . . 
+        . 4 d d e 4 4 4 e f . . 
+        . e d d e 1 1 1 1 f . . 
+        . f e e f 6 6 6 6 f f . 
+        . f f f f f f f f f f . 
+        . . f f . . . f f f . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . f f f e e e e e f . . 
+        f f f e e e e e e e f . 
+        f f f f e e e e e e e f 
+        f f f f f e e e 4 e e f 
+        f f f f e e e 4 4 e e f 
+        f f f f 4 4 4 4 4 e f f 
+        f f 4 e 4 f f 4 4 e f . 
+        . f 4 d 4 d d d d f . . 
+        . f f f e e d b b f . . 
+        . . f 4 d d e 4 e f . . 
+        . . f e d d e 1 1 f . . 
+        . f f f e e f 6 6 f f . 
+        . f f f f f f f f f f . 
+        . . f f . . . f f f . . 
+        `,img`
+        . . . . f f f f f . . . 
+        . . f f e e e e e f . . 
+        . f f e e e e e e e f . 
+        f f f f e e e e e e e f 
+        f f f f f e e e 4 e e f 
+        f f f f e e e 4 4 e e f 
+        f f f f 4 4 4 4 4 e f f 
+        f f 4 e 4 f f 4 4 e f f 
+        . f 4 d 4 d d d d f f . 
+        . f f f 4 d d b b f . . 
+        . . f e e 4 4 4 e f . . 
+        . . 4 d d e 1 1 1 f . . 
+        . . e d d e 1 1 1 f . . 
+        . . f e e f 6 6 6 f . . 
+        . . . f f f f f f . . . 
+        . . . . f f f . . . . . 
+        `],
+    100,
+    true
+    )
+})
+let Hero: Sprite = null
 scene.setBackgroundColor(9)
-let Hero = sprites.create(img`
+Hero = sprites.create(img`
     . . . . . . . . . . . . 
     . . . f f f f f f . . . 
     . f f f e e e e e f . . 
@@ -17,5 +140,9 @@ let Hero = sprites.create(img`
     . f f f f f f f f f f . 
     . . f f . . . f f f . . 
     `, SpriteKind.Player)
-controller.moveSprite(Hero)
+controller.moveSprite(Hero, 100, 0)
 tiles.setTilemap(tilemap`level1`)
+scene.cameraFollowSprite(Hero)
+// Gestisce la gravità 
+// 
+Hero.ay = 100
